579c6f59a82832d5874d4493685c4841eb397b30
TheScarastic <warabhishek@gmail.com>
2018-12-28 15:13:24 -0500

    cas: parts: Add DiracSound backend
    Special thanks to vasishath for his base work on mi3
    
    Change-Id: Id81e60a18dc34c991b82a5a2685c9041222a6ea7

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import android.util.Log;
 import androidx.preference.PreferenceManager;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.utils.FileUtils;
 
@@ -41,6 +42,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
 		RefreshUtils.startService(context);
 
