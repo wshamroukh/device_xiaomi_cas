18910a0a5a07d8ac3715934bff31ce1a12f5d955
someone5678 <nemui3353@gmail.com>
2022-11-07 13:59:56 +0900

    cas: Bluetooth: Rename libbthost_if to libbthost_if_sink * This fixes the following error: E a2dp_offload: open_a2dp_sink: DLOPEN failed for libbthost_if_sink.so
    * Also add libprocessgroup into LOCAL_SHARED_LIBRARIES
    
    Ref:
    https://github.com/LineageOS/android_device_razer_aura/commit/f63109de7a63ef508c9b7f5aedae076052fd4089

--- device.mk
+++ device.mk
@@ -165,6 +165,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth.a2dp@1.0-impl.mock \
     com.dsi.ant@1.0.vendor \
+    libbthost_if_sink \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
