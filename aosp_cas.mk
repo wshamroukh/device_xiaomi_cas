f770e45ca621fa927b767d2c446f55c8bd727359
wshamroukh <waddahs@microsoft.com>
2022-12-30 05:45:22 +0000

    PixelExp

--- aosp_cas.mk
+++ aosp_cas.mk
@@ -11,26 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := xtended_cas
+PRODUCT_NAME := aosp_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
 PRODUCT_MANUFACTURER := Xiaomi
 
-TARGET_INCLUDE_PIXEL_CHARGER := true
-
-# Xtended Stuffs
-XTENDED_BUILD_MAINTAINER := waddah
-XTENDED_BUILD_TYPE := UNOFFICIAL
-WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_BLUR := true
-XTENDED_BUILD_DONATE_URL := https://www.paypal.com/paypalme/Alec174
+TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
