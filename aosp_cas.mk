4cb46d164e2766d6304abbf7972290ed460c53b5
Waddah Shamroukh <waddahs@microsoft.com>
2023-01-06 10:46:35 +0400

    initial commit

--- aosp_cas.mk
+++ aosp_cas.mk
@@ -11,27 +11,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common PEX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+PEX_BUILD_TYPE := OFFICIAL
+PEX_MAINTAINER := WADDAH
 
-PRODUCT_NAME := evolution_cas
+PRODUCT_NAME := aosp_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
 PRODUCT_MANUFACTURER := Xiaomi
 BOARD_VENDOR := Xiaomi
 
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# FOD animations
-EXTRA_UDFPS_ANIMATIONS := true
-
-EVO_BUILD_TYPE := OFFICIAL
-TARGET_SUPPORTS_QUICK_TAP := true
-WITH_GMS := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="cas" \
+    PRODUCT_NAME="cas"
 
 BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.1.0.SJJCNXM:user/release-keys
