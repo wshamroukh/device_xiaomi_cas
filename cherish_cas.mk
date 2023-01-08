b7b0f0251994fa96e219436eeddd1bd0dd8fd2a3
Waddah Shamroukh <waddahs@microsoft.com>
2023-01-08 21:39:58 +0400

    MAINTAINER

--- cherish_cas.mk
+++ cherish_cas.mk
@@ -38,6 +38,6 @@ WITH_GMS := true
 
 CHERISH_BUILD_TYPE := UNOFFICIAL
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=Waddah
+    ro.cherish.maintainer=WADDAH
 
 BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.1.0.SJJCNXM:user/release-keys
