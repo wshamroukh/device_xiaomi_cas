59aadd906be1b2c7dd54c02d99b078e0f073ff0f
Waddah Shamroukh <waddahs@microsoft.com>
2023-01-12 13:56:07 +0400

    cas: Update blobs to V13.0.6.0.SJJCNXM

--- xtended_cas.mk
+++ xtended_cas.mk
@@ -34,4 +34,4 @@ XTENDED_BUILD_DONATE_URL := https://www.paypal.com/paypalme/Alec174
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.1.0.SJJCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.6.0.SJJCNXM:user/release-keys
