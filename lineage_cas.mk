8067021d31e4197be963c80f52ac5f353a3a1a79
Waddah Shamroukh <waddahs@microsoft.com>
2023-01-12 14:03:54 +0400

    cas: Update blobs to V13.0.6.0.SJJCNXM

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -29,4 +29,4 @@ TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.1.0.SJJCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.6.0.SJJCNXM:user/release-keys
