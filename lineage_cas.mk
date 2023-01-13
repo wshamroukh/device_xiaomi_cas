f0c72ed93605021e3632f81e9731966ced36f45f
Waddah Shamroukh <waddahs@microsoft.com>
2023-01-14 00:11:24 +0400

    rice tags update

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -14,8 +14,24 @@ $(call inherit-product, device/xiaomi/cas/device.mk)
 # Inherit some common rice stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Boot Animation
+
 TARGET_BOOT_ANIMATION_RES := 1080
+RICE_CHIPSET := Snapdragon 865
+RICE_MAINTAINER := WADDAH
+SUSHI_BOOTANIMATION := 1080
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := true
+TARGET_KERNEL_OPTIONAL_LD := false
+TARGET_USE_PIXEL_FINGERPRINT := false
+TARGET_GAPPS_ARCH := arm64
+TARGET_OPTOUT_GOOGLE_TELEPHONY := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+RICE_OFFICIAL := true
+WITH_GMS := true
 
 PRODUCT_NAME := lineage_cas
 PRODUCT_DEVICE := cas
@@ -26,44 +42,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.6.0.SJJCNXM:user/release-keys
-# chipset flag enclose var with "" if more than one
-# friendly tip: builders can use init_xxx.cpp as workaround for spacing
-# e.g. property_override("ro.rice.chipset", "Snapdragon 870 5G");
-RICE_CHIPSET := Snapdragon 865
 
-# chipset flag enclose var with "" if more than one
-# friendly tip: builders can use init_xxx.cpp as workaround for spacing
-# e.g. property_override("ro.rice.maintainer", "get riced");
-RICE_MAINTAINER := waddah
-
-# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
-SUSHI_BOOTANIMATION := 1080
-
-# Graphene Camera
-TARGET_BUILD_GRAPHENEOS_CAMERA := true
-
-# disable/enable blur support, default is false
-TARGET_ENABLE_BLUR := true
-
-# UDFPS ICONS/ANIMATIONS
-TARGET_HAS_UDFPS := true
-
-# Allow usage of custom binary linker (LD), default is false
-TARGET_KERNEL_OPTIONAL_LD := false
-
-# Spoof build description/fingerprint as pixel device
-TARGET_USE_PIXEL_FINGERPRINT := false
-
-TARGET_GAPPS_ARCH := arm64
-
-# Opt out of google dialer support, compiler will build arrow dialer,
-# package type will change from PIXEL -> GMS
-TARGET_OPTOUT_GOOGLE_TELEPHONY := false
-
-WITH_GMS := true
-
-# Quick Tap 
-TARGET_SUPPORTS_QUICK_TAP := true
-
-#Face unlock 
-TARGET_FACE_UNLOCK_SUPPORTED := true
