e309c43d419ab95ceb66f0dd65d421645fc01c97
wshamroukh <waddahs@microsoft.com>
2023-02-18 16:04:51 +0400

    cameraaux

--- device.mk
+++ device.mk
@@ -325,6 +325,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-system
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    ApertureRes \
     CarrierConfigResCommon \
     FrameworksResCommon \
     FrameworksResTarget \
