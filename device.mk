33550d6a3bcc9ac7d01756e0149eacd4c4217ace
wshamroukh <waddahs@microsoft.com>
2023-02-27 23:29:32 +0400

    Speed profile services and wifi-service to reduce RAM and storage

--- device.mk
+++ device.mk
@@ -391,6 +391,9 @@ PRODUCT_PACKAGES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
