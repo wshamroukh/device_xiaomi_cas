f491a1ad2501fddc2d0517e315627ca939809ea3
wshamroukh <waddahs@microsoft.com>
2023-03-02 11:54:42 +0400

    Implement powershare (reverse charging)

--- device.mk
+++ device.mk
@@ -358,6 +358,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.xiaomi_kona
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
