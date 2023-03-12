5d57e6b39cb36151d30e703a80fa6561dbb9be57
Waddah Shamroukh <waddahs@microsoft.com>
2023-03-12 20:09:39 +0400

    cherish bringup

---
+++ cherish_cas.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/xiaomi/cas/device.mk)
+
+# Inherit some common cherish stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Rom Specific Flags
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+EXTRA_UDFPS_ANIMATIONS := true
+WITH_GMS := true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Waddah
+CHERISH_BUILD_TYPE := UNOFFICIAL
+
+PRODUCT_NAME := cherish_cas
+PRODUCT_DEVICE := cas
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := M2007J1SC
+PRODUCT_MANUFACTURER := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ1A.230205.002 9471150 release-keys"
+
+BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ1A.230205.002/9471150:user/release-keys"
