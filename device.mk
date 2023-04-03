4a6ad6ad35a11cc33b431ac3d9557cf56d006055
wshamroukh <waddahs@microsoft.com>
2023-04-03 20:58:00 +0000

    add thermal package

--- device.mk
+++ device.mk
@@ -418,6 +418,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+	android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@2.0-service.qti \
     android.hardware.thermal@2.0
 
