622c88824c3d83b5403f4b32e2e127d05f943298
wshamroukh <waddahs@microsoft.com>
2023-05-23 12:31:26 +0000

    ProjectElixir Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_cas.mk
+    $(LOCAL_DIR)/aosp_cas.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_cas-eng \
-    evolution_cas-user \
-    evolution_cas-userdebug
+    aosp_cas-eng \
+    aosp_cas-user \
+    aosp_cas-userdebug
