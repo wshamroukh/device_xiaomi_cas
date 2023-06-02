6bd2360b753180f445c45fdc1ccebe43b58f3611
wshamroukh <waddahs@microsoft.com>
2023-06-02 05:58:35 +0000

    cas: parts: HBM Fix compilation for QPR1 merge

--- parts/src/org/lineageos/settings/hbm/HBMActivity.java
+++ parts/src/org/lineageos/settings/hbm/HBMActivity.java
@@ -23,7 +23,7 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class HBMActivity extends CollapsingToolbarBaseActivity {
 
