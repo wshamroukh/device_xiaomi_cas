afb3bf98c52c21a87953622434b7459176136c8b
wshamroukh <waddahs@microsoft.com>
2023-08-18 21:12:28 +0000

    init SuperiorExtended

--- superior_cas.mk
+++ superior_cas.mk
@@ -11,20 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-EVO_BUILD_TYPE := COMMUNITY
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit some common superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+SUPERIOR_OFFICIAL := false
+IS_PHONE := true
+BUILD_WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_MINI_GAPPS := false
+TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_SUPPORTS_CALL_RECORDING := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_INCLUDE_WIFI_EXT := true
+SUPERIOR_UDFPS_ANIMATIONS := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
 
-PRODUCT_NAME := evolution_cas
+PRODUCT_NAME := superior_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
