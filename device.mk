384eda9f9652d41c2331f687c2f44a770fed12e7
wshamroukh <waddahs@microsoft.com>
2023-08-27 15:28:26 +0400

    FastCharge Implementation

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.xiaomi
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
