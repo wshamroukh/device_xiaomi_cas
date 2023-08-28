8d8defb1702e9b1dcf1744de6cf3532a338f07be
wshamroukh <waddahs@microsoft.com>
2023-08-28 19:07:09 +0000

    Revert "cas: parts: Adapt Thermal Parts to S style"
    This reverts commit 1b19015abfe6ea58ea4e8a29ef0cdf8a123b0da3.

--- parts/src/org/lineageos/settings/thermal/ThermalSettingsFragment.java
+++ parts/src/org/lineageos/settings/thermal/ThermalSettingsFragment.java
@@ -103,7 +103,6 @@ public class ThermalSettingsFragment extends PreferenceFragment
     @Override
     public void onResume() {
         super.onResume();
-        getActivity().setTitle(getResources().getString(R.string.thermal_title));
         rebuild();
     }
 
