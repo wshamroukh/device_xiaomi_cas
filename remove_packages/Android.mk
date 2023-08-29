1672d0fb68c4faa775746e072ed9da9aae5bc97c
wshamroukh <waddahs@microsoft.com>
2023-08-29 07:01:22 +0000

    cas: Update Remove Packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -1,15 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := RemovePackages
+LOCAL_MODULE := CasRemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CalendarGooglePrebuilt
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += AmbientStreaming
-LOCAL_OVERRIDES_PACKAGES += VZWAPNLib
+LOCAL_OVERRIDES_PACKAGES := \
+          CalendarGooglePrebuilt \
+		  Duo \
+		  YTMusic \
+		  CalendarGoogle \
+		  YouTube \
+		  Chrome 
+		  
+
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
