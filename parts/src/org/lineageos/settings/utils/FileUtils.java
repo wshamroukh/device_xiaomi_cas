5c749162f65fce26a24dcba30decfbb835c74f84
wshamroukh <waddahs@microsoft.com>
2023-08-30 16:09:33 +0400

    Revert "cas: parts: Implement HBM & Auto HBM"
    This reverts commit 1c9016ef8106dfb170782add162d7a92ff3da02f.

--- parts/src/org/lineageos/settings/utils/FileUtils.java
+++ parts/src/org/lineageos/settings/utils/FileUtils.java
@@ -16,10 +16,6 @@
 
 package org.lineageos.settings.utils;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.UserHandle;
-
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -29,9 +25,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
-import org.lineageos.settings.hbm.HBMFragment;
-import org.lineageos.settings.hbm.AutoHBMService;
 
 public final class FileUtils {
     private static final String TAG = "FileUtils";
@@ -163,27 +156,5 @@ public final class FileUtils {
             Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
         }
         return ok;
-    }
-
-    private static boolean mServiceEnabled = false;
-
-    private static void startService(Context context) {
-        context.startServiceAsUser(new Intent(context, AutoHBMService.class),
-                UserHandle.CURRENT);
-        mServiceEnabled = true;
-    }
-
-    private static void stopService(Context context) {
-        mServiceEnabled = false;
-        context.stopServiceAsUser(new Intent(context, AutoHBMService.class),
-                UserHandle.CURRENT);
-    }
-
-    public static void enableService(Context context) {
-        if (HBMFragment.isAUTOHBMEnabled(context) && !mServiceEnabled) {
-            startService(context);
-        } else if (!HBMFragment.isAUTOHBMEnabled(context) && mServiceEnabled) {
-            stopService(context);
-        }
     }
 }
