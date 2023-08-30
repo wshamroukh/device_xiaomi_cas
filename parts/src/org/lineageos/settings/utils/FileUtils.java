f4030a3d9e4d37e5b03ed56d8d7f139ec01018ef
wshamroukh <waddahs@microsoft.com>
2023-08-30 16:04:14 +0400

    Revert "cas: parts: Fix enableHBM boolean logic"
    This reverts commit 5d99230826ed6da058f6b06e87df72ffc84957f5.

--- parts/src/org/lineageos/settings/utils/FileUtils.java
+++ parts/src/org/lineageos/settings/utils/FileUtils.java
@@ -165,22 +165,6 @@ public final class FileUtils {
         return ok;
     }
 
-    public static boolean getFileValueAsBoolean(String filename, boolean defValue) {
-        String fileValue = readOneLine(filename);
-        if(fileValue!=null){
-            return (fileValue.equals("0")?false:true);
-        }
-        return defValue;
-    }
-
-    public static String getFileValue(String filename, String defValue) {
-        String fileValue = readOneLine(filename);
-        if(fileValue!=null){
-            return fileValue;
-        }
-        return defValue;
-    }
-
     private static boolean mServiceEnabled = false;
 
     private static void startService(Context context) {
