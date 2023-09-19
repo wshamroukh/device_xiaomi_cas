5a7c7264326a0a3d8fd42c2a3d7890f3160d7518
wshamroukh <waddahs@microsoft.com>
2023-09-19 12:13:04 +0000

    cas: RemovePackages: Remove unnecessary packages - Youtube - Maps

--- device.mk
+++ device.mk
@@ -396,6 +396,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.6.vendor \
