a0352de1779384890765147df19fc752cad128e7
wshamroukh <waddahs@microsoft.com>
2023-10-14 16:46:58 +0000

    cas: Switch to QTI AV component

--- device.mk
+++ device.mk
@@ -272,8 +272,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libavservices_minijail \
-    libavservices_minijail.vendor
+    libavservices_minijail
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
@@ -388,6 +387,7 @@ PRODUCT_COPY_FILES += \
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
+	av \
     perf
 
 # RIL
