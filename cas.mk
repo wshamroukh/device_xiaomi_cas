41131c4dc3dad9da954a04f436fc0d808ee8522c
wshamroukh <waddahs@microsoft.com>
2023-10-19 20:10:51 +0000

    cas: Zephyrus bringup

---
+++ cas.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/xiaomi/cas/device.mk)
+
+# Zephyrus flags
+CUSTOM_BUILD_TYPE := Official
+WITH_GMS := true
+SHIP_APERTURE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER_IMAGES := true
+TARGET_SUPPORTS_BLUR := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
+PRODUCT_NAME := cas
+PRODUCT_DEVICE := cas
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := M2007J1SC
+PRODUCT_MANUFACTURER := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
