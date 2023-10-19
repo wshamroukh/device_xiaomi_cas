14c135a2c02ae4ab452e1a0b56e6e6ec82dfd442
wshamroukh <waddahs@microsoft.com>
2023-10-19 20:17:58 +0000

    cas: Switch to AIDL wifi service

--- device.mk
+++ device.mk
@@ -465,7 +465,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
