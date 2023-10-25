9dcea3d7e869a4ca419bbbeaf00afa36137e4459
wshamroukh <waddahs@microsoft.com>
2023-10-25 18:44:58 +0000

    cas: disable Atrace tracefs must be mounted at either/sys/kernel/tracing or /sys/kernel/debug/tracing

--- device.mk
+++ device.mk
@@ -72,8 +72,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
+#PRODUCT_PACKAGES += \
+#    android.hardware.atrace@1.0-service
 
 # Audio
 PRODUCT_PACKAGES += \
