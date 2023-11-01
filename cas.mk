790def405a68cb88ce7be04552f83ee59f919bd8
wshamroukh <waddahs@microsoft.com>
2023-11-01 08:39:28 +0000

    cas: Welcome Zephyrus

--- cas.mk
+++ cas.mk
@@ -11,25 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-EVO_BUILD_TYPE := COMMUNITY
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
+# Zephyrus flags
+CUSTOM_BUILD_TYPE := Official
+WITH_GMS := true
+GMS_FULL := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_MINI_GAPPS := false
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_INCLUDES_MIUI_CAMERA := true
-TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER_IMAGES := true
+TARGET_SUPPORTS_BLUR := true
 SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_MIUI_CAMERA := true
+TARGET_INCLUDES_MIUI_CAMERA := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := true
 
-PRODUCT_NAME := evolution_cas
+PRODUCT_NAME := cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
@@ -38,4 +34,4 @@ BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-#BUILD_FINGERPRINT := Xiaomi/cas/cas:13/RKQ1.211001.001/V14.0.2.0.TJJCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cas/cas:13/RKQ1.211001.001/V14.0.2.0.TJJCNXM:user/release-keys
