2d3fc01997a86052d54d1fb993feeb2cb2514bbf
wshamroukh <waddahs@microsoft.com>
2023-11-11 14:43:56 +0000

    Revert "Build libbthost_if & libldacBT_bco to vendor"
    This reverts commit 62d7f84b763b5424de7288d799bbf8bb3301e47e.

--- device.mk
+++ device.mk
@@ -150,9 +150,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio-impl \
     com.dsi.ant@1.0.vendor \
-    libbthost_if \
-    libbthost_if.vendor \
-    libldacBT_bco.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
