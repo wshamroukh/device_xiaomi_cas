512b18a2c1ab8fb7806aa3acd729b4f807bb6b65
wshamroukh <waddahs@microsoft.com>
2023-11-12 08:19:14 +0000

    cas: Build android.hardware.tetheroffload.config@1.0 * This fixes the following error: I hwservicemanager: getTransport: Cannot find entry android.hardware.tetheroffload.config@1.0::IOffloadConfig/default in either framework or device VINTF manifest. E HidlServiceManagement: Service android.hardware.tetheroffload.config@1.0::IOffloadConfig/default must be in VINTF manifest in order to register/get. E IPAHALService: Failed to register IOffloadConfig (-2147483648)
    * With this commit now we see the following:
    I HidlServiceManagement: Registered android.hardware.tetheroffload.config@1.0::IOffloadConfig/default
    I IPAHALService: Successfully registered IOffloadConfig

--- device.mk
+++ device.mk
@@ -472,6 +472,7 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.tetheroffload.config@1.0 \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
