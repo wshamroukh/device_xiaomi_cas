18a472773002dd8e89f70e324cee9d5807fbf9b3
wshamroukh <waddahs@microsoft.com>
2023-11-15 15:35:21 +0000

    cas: init: Remove legacy ril-daemon rules W libc    : Unable to set property ctl.stop to vendor.ril-daemon: error code: 0x20

--- init/init.class_main.sh
+++ init/init.class_main.sh
@@ -41,7 +41,6 @@ case "$baseband" in
 #    setprop ro.vendor.radio.noril yes
     setprop ro.vendor.radio.noril true
     stop ril-daemon
-    stop vendor.ril-daemon
     stop vendor.qcrild
 esac
 
@@ -88,11 +87,7 @@ case "$baseband" in
     if [ "$qcrild_status" = "true" ]; then
         # Make sure both rild, qcrild are not running at same time.
         # This is possible with vanilla aosp system image.
-        stop vendor.ril-daemon
-
         start vendor.qcrild
-    else
-        start vendor.ril-daemon
     fi
 
     case "$baseband" in
