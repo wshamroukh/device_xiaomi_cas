2a5e8422939bed9d7b827d5400b1cc4fceaafe56
wshamroukh <waddahs@microsoft.com>
2023-12-07 08:22:09 +0000

    cas: Build android.frameworks.sensorservice@1.0 W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL (if it's not configured to be a lazy HAL, it may be stuck starting or still starting). I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default... W libc    : Unable to set property ctl.interface_start to android.frameworks.sensorservice@1.0::ISensorManager/default: error code: 0x20 I hwservicemanager: Tried to start android.frameworks.sensorservice@1.0::ISensorManager/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.

--- device.mk
+++ device.mk
@@ -396,6 +396,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
