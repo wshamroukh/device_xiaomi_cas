7a2105dde98fb29339dd6f3e40845209fdeb4825
wshamroukh <waddahs@microsoft.com>
2023-12-14 06:52:06 +0000

    Revert "FastCharge Implementation"
    This reverts commit 4f9ef59204f54ad2aade3f219157cdcd30c62829.

--- device.mk
+++ device.mk
@@ -213,10 +213,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.xiaomi
-
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
