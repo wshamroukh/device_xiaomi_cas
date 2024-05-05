c157c40a3818181ddec74f3dbaeaf22fcb678318
wshamroukh <wshamroukh@gmail.com>
2024-05-05 11:09:03 +0000

    cas: Initialize Project Matrixx

---
+++ lineage_cas.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/xiaomi/cas/device.mk)
+
+# Inherit some common Project Matrixx stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+MATRIXX_BUILD_TYPE := Unofficial
+MATRIXX_MAINTAINER := Waddah
+MATRIXX_CHIPSET := SM8250
+MATRIXX_BATTERY := 4500mah
+MATRIXX_DISPLAY := 1080x2340
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_INCLUDES_MIUI_CAMERA := true
+TARGET_USES_MIUI_CAMERA := true
+WITH_SU := true
+WITH_GMS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
+
+PRODUCT_NAME := lineage_cas
+PRODUCT_DEVICE := cas
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := M2007J1SC
+PRODUCT_MANUFACTURER := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Xiaomi/cas/cas:13/RKQ1.211001.001/V14.0.2.0.TJJCNXM:user/release-keys
