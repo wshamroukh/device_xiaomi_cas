c56137c2dd504f2ee9ab4444e42ae8b19ddb9869
wshamroukh <wshamroukh@gmail.com>
2024-05-05 17:39:20 +0000

    Revert "cas: fastcharge: Extend Fastcharge implementation"
    This reverts commit e82e3db59f86094842cc8c79b35b5f7eaba069f7.

--- fastcharge/service.cpp
+++ fastcharge/service.cpp
@@ -20,41 +20,31 @@
 #include <hidl/HidlTransportSupport.h>
 
 #include "FastCharge.h"
-#include "RestrictedCurrent.h"
 
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 using vendor::lineage::fastcharge::V1_0::IFastCharge;
 using vendor::lineage::fastcharge::V1_0::implementation::FastCharge;
-using vendor::lineage::fastcharge::V1_0::implementation::RestrictedCurrent;
 
 using android::OK;
 using android::status_t;
 
 int main() {
-  android::sp<FastCharge> scharge = new FastCharge();
-  android::sp<RestrictedCurrent> scurrent = new RestrictedCurrent();
+  android::sp<FastCharge> service = new FastCharge();
 
   configureRpcThreadpool(1, true);
 
-  status_t status = scharge->registerAsService();
+  status_t status = service->registerAsService();
   if (status != OK) {
-    LOG(ERROR) << "Could not register service for FastCharge HAL FastCharge Iface.";
+    LOG(ERROR) << "Cannot register FastCharge HAL service.";
     return 1;
-  }
-
-  status = scurrent->registerAsService();
-  if (status != OK) {
-      LOG(ERROR) << "Could not register service for FastCharge HAL RestrictedCurrent Iface.";
-      return 1;
   }
 
   LOG(INFO) << "FastCharge HAL service ready.";
 
   joinRpcThreadpool();
 
-  // In normal operation, we don't expect the thread pool to shutdown
-  LOG(ERROR) << "FastCharge HAL service is shutting down.";
+  LOG(ERROR) << "FastCharge HAL service failed to join thread pool.";
   return 1;
 }
