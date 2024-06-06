29aed85da7ea87229cd330eb154b5ee9db5edb6e
wshamroukh <wshamroukh@gmail.com>
2024-06-06 08:00:15 +0000

    Revert "cas: Import missing NFC config"
    This reverts commit c7d2395a0c79d82dbf04612aa3245e2b76066380.

--- device.mk
+++ device.mk
@@ -330,8 +330,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
