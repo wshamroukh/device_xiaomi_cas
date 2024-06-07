bd2d9c80404b53b730c0d4be1622dc0815bdebc6
wshamroukh <wshamroukh@gmail.com>
2024-06-07 07:59:46 +0000

    cas: Uprev android.hardware.audio.effect - We are seeing these errors: I hwservicemanager: getTransport: Cannot find entry android.hardware.audio.effect@7.0::IEffectsFactory/default in either framework or device VINTF manifest. E LegacySupport: Could not get passthrough implementation for android.hardware.audio.effect@7.0::IEffectsFactory/default.
    - After this commit, we see:
    I HidlServiceManagement: Registered android.hardware.audio.effect@7.0::IEffectsFactory/default
    I LegacySupport: Registration complete for android.hardware.audio.effect@7.0::IEffectsFactory/default.

--- device.mk
+++ device.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
     android.hardware.soundtrigger@2.3-impl \
