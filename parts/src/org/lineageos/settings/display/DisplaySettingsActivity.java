ad21fe08bb2462b6917bae5758e823e1eaf7ecf7
Peter Kalauskas <Peter Kalauskas@users.noreply.gihub.com>
2024-06-28 10:45:31 +0000

    cas: parts: Fix building for A14 QPR2
    * Enable use_resource_processor for all sysui deps
    * com.android.settingslib.widget.R is no longer available to be imported
    
    Co-authored-by: Peter Kalauskas <Peter Kalauskas@users.noreply.gihub.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- parts/src/org/lineageos/settings/display/DisplaySettingsActivity.java
+++ parts/src/org/lineageos/settings/display/DisplaySettingsActivity.java
@@ -20,7 +20,6 @@ package org.lineageos.settings.display;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class DisplaySettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,7 +28,7 @@ public class DisplaySettingsActivity extends CollapsingToolbarBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new DisplaySettingsFragment(), TAG_DCDIMMING).commit();
     }
 }
