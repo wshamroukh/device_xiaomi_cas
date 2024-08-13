1e364f3cb087d3c6119c8943883ab6436590beb1
wshamroukh <wshamroukh@gmail.com>
2024-08-13 10:44:13 +0000

    cas: Update fingerprint to V816.0.4.0.TJJCNXM

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -46,4 +46,4 @@ BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cas/cas:13/RKQ1.211001.001/V14.0.2.0.TJJCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cas/cas:13/RKQ1.211001.001/V816.0.4.0.TJJCNXM:user/release-keys
