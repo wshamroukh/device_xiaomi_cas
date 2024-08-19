0c615afa32e8d8fd060b1ee494c34a789277ff36
wshamroukh <wshamroukh@gmail.com>
2024-08-19 09:19:55 +0000

    cas: drop android.hardware.atrace This was causing the device to gets stuck in bootloop - This crash log was logged in pmsg-ramoops-0.log:
    DEBUG *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
    DEBUG Matrixx Version: '10.7.1'
    DEBUG Build fingerprint: 'Xiaomi/cas/cas:13/RKQ1.211001.001/V816.0.4.0.TJJCNXM:user/release-keys'
    DEBUG Revision: '0'
    DEBUG ABI: 'arm64'
    DEBUG Timestamp: 1970-01-01 17:29:49.083000477+0000
    DEBUG Process uptime: 1s
    DEBUG Cmdline: /vendor/bin/hw/android.hardware.atrace@1.0-service
    DEBUG pid: 1775, tid: 1775, name: android.hardwar  >>> /vendor/bin/hw/android.hardware.atrace@1.0-service <<<
    DEBUG uid: 1000
    DEBUG signal 6 (SIGABRT), code -1 (SI_QUEUE), fault addr --------
    DEBUG Abort message: 'Check failed: stat(tracefs_event_root_.c_str(), &st) == 0 tracefs must be mounted at either/sys/kernel/tracing or /sys/kernel/debug/tracing'
    DEBUG     x0  0000000000000000  x1  00000000000006ef  x2  0000000000000006  x3  0000007fcfc2fc40
    DEBUG     x4  736d6475645e7265  x5  736d6475645e7265  x6  736d6475645e7265  x7  7f7f7f7f7f7f7f7f
    DEBUG     x8  00000000000000f0  x9  00000000000006ef  x10 ffffff80ffffffdf  x11 fffffffffffffffd
    DEBUG     x12 0000007fcfc2fbe0  x13 000000000000008c  x14 b400007c58c19000  x15 00000000341555ad
    DEBUG     x16 0000007c59349750  x17 0000007c5932d780  x18 0000007c5cf20000  x19 00000000000006ef
    DEBUG     x20 00000000000006ef  x21 00000000ffffffff  x22 0000007c5c72f580  x23 0000000000000000
    DEBUG     x24 0000000000000000  x25 0000000000000000  x26 0000000000000000  x27 0000000000000000
    DEBUG     x28 0000000000000000  x29 0000007fcfc2fcc0
    DEBUG     lr  0000007c592bf8e8  sp  0000007fcfc2fc20  pc  0000007c592bf918  pst 0000000000001000
    DEBUG 6 total frames
    DEBUG backtrace:
    DEBUG       #00 pc 00000000000b2918  /apex/com.android.runtime/lib64/bionic/libc.so (abort+168) (BuildId: 44957d9e2518d7f3dc62da8092b3b620)
    DEBUG       #01 pc 000000000000616c  /system/lib64/liblog.so (__android_log_default_aborter+12) (BuildId: a972f74981fe305d6f747d6e9cb5d647)
    DEBUG       #02 pc 0000000000016ca0  /vendor/lib64/libbase.so (android::base::LogMessage::~LogMessage()+352) (BuildId: 9803b61a330c11bea312d1e317ecd2ab)
    DEBUG       #03 pc 00000000000035bc  /vendor/bin/hw/android.hardware.atrace@1.0-service (android::hardware::atrace::V1_0::implementation::AtraceDevice::AtraceDevice()+316) (BuildId: 68bde14b5a529630bcbeb5ed220b14f0)
    DEBUG       #04 pc 0000000000004da8  /vendor/bin/hw/android.hardware.atrace@1.0-service (main+40) (BuildId: 68bde14b5a529630bcbeb5ed220b14f0)
    DEBUG       #05 pc 00000000000abea4  /apex/com.android.runtime/lib64/bionic/libc.so (__libc_init+116) (BuildId: 44957d9e2518d7f3dc62da8092b3b620)

--- device.mk
+++ device.mk
@@ -94,10 +94,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
