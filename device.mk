c088fda54f6e82be4745f96cedb0a1c990ec2e4f
¨raystef66¨ <s.vanbarel@gmail.com>
2024-10-20 11:39:40 +0200

    cas: build libcrypto-33 * CANNOT LINK EXECUTABLE "/odm/bin/hw/android.hardware.drm@1.3-service.widevine": cannot locate symbol "CBS_init" referenced by "/vendor/lib64/libwvhidl.so"

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    libcrypto-v33
 
 # Fastboot
 PRODUCT_PACKAGES += \
