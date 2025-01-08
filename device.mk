0af3c08fadf113305de1dd8caff71160f41c511c
wshamroukh <wshamroukh@gmail.com>
2025-01-08 15:40:00 +0000

    cas: Switch to AIDL USB HAL * This fixes the issue with the USB and MTP screen where it is greyed out

--- device.mk
+++ device.mk
@@ -485,7 +485,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
