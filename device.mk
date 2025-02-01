d16bd231b742ecc530a0d0613bb6eef0d30359d9
wshamroukh <wshamroukh@gmail.com>
2025-02-01 14:03:02 +0000

    cas: Build android.hardware.keymaster@4.0
    init: Control message: Could not find 'android.hardware.keymaster@4.0::IKeymasterDevice/default' for ctl.interface_start from pid: 558 (/system/bin/hwservicemanager)

--- device.mk
+++ device.mk
@@ -289,7 +289,9 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1.vendor
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
 
 # LZ4
 PRODUCT_PACKAGES += \
