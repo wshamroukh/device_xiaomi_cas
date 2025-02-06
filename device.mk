e067547cc5e1ce33f0724239affd5ee34b5ca281
wshamroukh <wshamroukh@gmail.com>
2025-02-06 11:20:46 +0000

    cas: Switch to AIDL memtrack HIDL was not starting: I hwservicemanager: getTransport: Cannot find entry android.hardware.memtrack@1.0::IMemtrack/default in either framework or device VINTF manifest. E HidlServiceManagement: Service android.hardware.memtrack@1.0::IMemtrack/default must be in VINTF manifest in order to register/get. E LegacySupport: Could not register service android.hardware.memtrack@1.0::IMemtrack/default (-2147483648).

--- device.mk
+++ device.mk
@@ -183,8 +183,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service
 
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack-service.example \
     memtrack.kona
 
 PRODUCT_PACKAGES += \
