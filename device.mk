4814b4d39bccd71bbe2db71024df2c03880db940
wshamroukh <wshamroukh@gmail.com>
2025-02-11 07:18:18 +0000

    cas: Drop dummy libldacBT_bco It is not working, we are still getting the error I bluetooth-a2dp: system/stack/a2dp/a2dp_vendor_ldac_decoder.cc:125 A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found

--- device.mk
+++ device.mk
@@ -163,7 +163,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0-impl.mock \
     com.dsi.ant@1.0.vendor \
     libbthost_if_sink \
-    libldacBT_bco \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
