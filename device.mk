8124c448c81b571856d12d188756a728b588492a
wshamroukh <wshamroukh@gmail.com>
2025-06-16 07:22:17 +0000

    cas: Drop Powershare It was causing Quick settings to crash when trying to edit QS E vendor.lineage.powershare-service.default: Failed to read current PowerShare state E AndroidRuntime: FATAL EXCEPTION: main E AndroidRuntime: Process: com.android.systemui, PID: 13599 E AndroidRuntime: java.lang.UnsupportedOperationException: E AndroidRuntime: 	at android.os.Parcel.createExceptionOrNull(Parcel.java:3273) E AndroidRuntime: 	at android.os.Parcel.createException(Parcel.java:3245) E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:3228) E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:3170) E AndroidRuntime: 	at vendor.lineage.powershare.IPowerShare$Stub$Proxy.isEnabled(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:26) E AndroidRuntime: 	at com.android.systemui.qs.tiles.PowerShareTile.refreshState(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:32) E AndroidRuntime: 	at com.android.systemui.qs.tileimpl.QSTileImpl.onPowerSaveChanged(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:1) E AndroidRuntime: 	at com.android.systemui.statusbar.policy.BatteryControllerImpl.addCallback(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:28) E AndroidRuntime: 	at com.android.systemui.qs.tiles.PowerShareTile.<init>(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:132) E AndroidRuntime: 	at com.android.systemui.dagger.DaggerReferenceGlobalRootComponent$ReferenceSysUIComponentImpl$SwitchingProvider.get10(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:71) E AndroidRuntime: 	at com.android.systemui.dagger.DaggerReferenceGlobalRootComponent$ReferenceSysUIComponentImpl$SwitchingProvider.get(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:46) E AndroidRuntime: 	at com.android.systemui.qs.tileimpl.QSFactoryImpl.createTile(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:17) E AndroidRuntime: 	at com.android.systemui.qs.customize.QSCustomizerController.show(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:348) E AndroidRuntime: 	at com.android.systemui.qs.QSPanelController$$ExternalSyntheticLambda0.run(go/retraceme 02748d0606cbfc8c75ad106f59c08eb40397570d9e43e7162a05e62d33e2ef2b:37) E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:991) E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:102) E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:232) E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:317) E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8934) E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method) E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:591) E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:911)

--- device.mk
+++ device.mk
@@ -35,9 +35,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Powershare
-$(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
-
 # Platform
 PRODUCT_BOARD_PLATFORM := kona
 
@@ -434,10 +431,6 @@ PRODUCT_PACKAGES += \
     libjson \
     libsqlite.vendor \
     libsqlite.vendor:64
-
-# PowerShare
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare-service.default
 
 # QTI
 PRODUCT_PACKAGES += \
