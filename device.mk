5f8b073bcdcf85a38ffbddec893bb774bf049705
wshamroukh <wshamroukh@gmail.com>
2025-06-19 08:46:41 +0000

    cas: Build android.hardware.audio.sounddose-vendor-impl
    audiohalservice Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found

--- device.mk
+++ device.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
+    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.primary.kona \
