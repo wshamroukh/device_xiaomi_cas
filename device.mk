e363a1f5705afcc271dd2d862feda9d1720ac271
wshamroukh <wshamroukh@gmail.com>
2025-07-25 07:34:48 +0000

    cas: Build xiaomi-telephony-stub To provide the required Xiaomi IMS methods

--- device.mk
+++ device.mk
@@ -485,10 +485,12 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     qti-telephony-utils-prd \
     qti_telephony_utils_prd.xml \
-    telephony-ext
+    telephony-ext \
+    xiaomi-telephony-stub
 
 PRODUCT_BOOT_JARS += \
-    telephony-ext
+    telephony-ext \
+    xiaomi-telephony-stub
 
 # Thermal
 PRODUCT_PACKAGES += \
