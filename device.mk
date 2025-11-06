dd25b6508bde77ff288a524e88f7dcaab95814ad
wshamroukh <wshamroukh@gmail.com>
2025-11-06 06:09:14 +0000

    cas: Build NFC NXP HAL

--- device.mk
+++ device.mk
@@ -364,10 +364,9 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
+    android.hardware.nfc-service.nxp \
+    android.hardware.secure_element-service.nxp \
     com.android.nfc_extras \
-    libchrome.vendor \
-    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
