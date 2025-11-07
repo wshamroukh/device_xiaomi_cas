d2edb159c38b733dddc8f6c184f6902faacd4c96
wshamroukh <wshamroukh@gmail.com>
2025-11-07 16:38:34 +0000

    Revert "cas: Build NFC NXP HAL"
    This reverts commit dd25b6508bde77ff288a524e88f7dcaab95814ad.

--- device.mk
+++ device.mk
@@ -366,9 +366,10 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.nxp \
-    android.hardware.secure_element-service.nxp \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
+    libchrome.vendor \
+    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
