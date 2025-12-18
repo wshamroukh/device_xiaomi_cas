2542af5cd969bce5b462d2073358f966b2e914a4
wshamroukh <wshamroukh@gmail.com>
2025-12-18 06:06:13 +0000

    Revert "cas: Build android.hardware.audio.sounddose-vendor-impl"
    This reverts commit 5f8b073bcdcf85a38ffbddec893bb774bf049705.

--- device.mk
+++ device.mk
@@ -96,7 +96,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
-    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.primary.default \
