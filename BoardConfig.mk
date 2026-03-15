ce2c049ffe6179c0ab8421e6215afbbda6f35c9b
wshamroukh <wshamroukh@gmail.com>
2026-03-15 16:58:05 +0000

    cas: Enable Lineage Health FastCharge

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,11 +134,16 @@ TARGET_KERNEL_CONFIG := cas_defconfig
 KERNEL_CC := CC=clang
 override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
 
-# Lineage Health
+# Lineage Health ChargingControl
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+
+# Lineage Health FastCharge
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/qcom-battery/restrict_chg)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
